90a71db413bbcdfab12a7902ea9ed0b1b0e88c8b
Felipe Florez Olivera <114842657+FelipeFlorezO@users.noreply.github.com>
2023-02-28 16:34:54 -0500

    Add files via upload

---
+++ dimensionesejemplo.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+scalar = np.array(42)
+print(scalar)
+scalar.ndim
+
+vector = np.array([1,2,3])
+print(vector)
+vector.ndim
+
+matriz = np.array([[1,2,3],[4,5,6]])
+print(matriz)
+matriz.ndim
+
+tensor = np.array([[[1,2,3],[4,5,6,],[7,8,9]]])
+print(tensor)
+tensor.ndim
+
+
+
+
+
